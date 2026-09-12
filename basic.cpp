a0aef100e7aa35a04b975781dc510bca63f7efc0
sneha-goyal7 <snehagoyal4295@gmail.com>
2026-09-12 16:33:26 +0530

    back to basic again

---
+++ basic.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+    //   int n;
+    //   int sum=0;
+    //   cin>>n;
+    //   for(int i=1;i<=n;i++){
+    //     sum+=i;
+    //   }
+    //   cout<<sum;
+    int n=9;
+    bool isPrime=true;
+    if(n<=1){
+        isPrime=false;
+    }
+    for(int i=2;i<=sqrt(n);i++){
+        if(n%i==0){
+            isPrime=false;
+            break;
+        }
+    }
+    if(isPrime)
+    cout<<"number is prime";
+    else 
+    cout<<"number is not prime";
+    return 0;
+}
