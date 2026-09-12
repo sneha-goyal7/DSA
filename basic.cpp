193129b3057126f16d2a95e69c4e2f335e830fb3
sneha-goyal7 <snehagoyal4295@gmail.com>
2026-09-12 16:39:11 +0530

    start function

--- basic.cpp
+++ basic.cpp
@@ -10,20 +10,26 @@ int main(){
     //     sum+=i;
     //   }
     //   cout<<sum;
-    int n=9;
-    bool isPrime=true;
-    if(n<=1){
-        isPrime=false;
+    // int n=9;
+    // bool isPrime=true;
+    // if(n<=1){
+    //     isPrime=false;
+    // }
+    // for(int i=2;i<=sqrt(n);i++){
+    //     if(n%i==0){
+    //         isPrime=false;
+    //         break;
+    //     }
+    // }
+    // if(isPrime)
+    // cout<<"number is prime";
+    // else 
+    // cout<<"number is not prime";
+    int n=5;
+    int fact=1;
+    for(int i=1;i<=n;i++){
+        fact*=i;
     }
-    for(int i=2;i<=sqrt(n);i++){
-        if(n%i==0){
-            isPrime=false;
-            break;
-        }
-    }
-    if(isPrime)
-    cout<<"number is prime";
-    else 
-    cout<<"number is not prime";
+    cout<<fact;
     return 0;
 }
