4832fc3af7da0e8170586e922f2fc87fb6354253
sneha-goyal7 <snehagoyal4295@gmail.com>
2026-09-13 15:06:14 +0530

    changes

--- INT_MIN_MAX.cpp
+++ INT_MIN_MAX.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include <climits>
+// #include<algorithm>
 using namespace std;
 void findminmax(int arr[], int size)
 {
     int smallest = INT_MAX;
     int largest = INT_MIN;
-    int smallest = -1;
-    int largest = -1;
+    // int smallest = -1;
+    // int largest = -1;
     for (int i = 0; i < size; i++)
     {
         smallest = min(arr[i], smallest);
+        
         largest = max(arr[i], largest);
     }
     cout << smallest << largest;
