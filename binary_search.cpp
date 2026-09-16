f3b61d602cc87c3e4192eaf36dde646fd5eae949
sneha-goyal7 <snehagoyal4295@gmail.com>
2026-09-16 15:44:45 +0530

    binary search

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include<vector>
 using namespace std;
-
+int bs(vector<int>arr,int tar){
+    int st=0,end=arr.size()-1;
+    while(st<=end){
+        int mid=(st+end)/2;
+        if(tar==arr[mid])return mid;
+        else if(tar<arr[mid])end=mid-1;
+        else st=mid+1;
+    }
+    return -1;
+}
 int main(){
-    
+    vector<int>arr={2,4,6,7,9,10};
+    cout<<bs(arr,7);
     return 0;
 }
