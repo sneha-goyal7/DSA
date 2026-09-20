d1101614eaebc4074b58428cd27b1e2881ed4a0b
sneha-goyal7 <snehagoyal4295@gmail.com>
2026-09-20 15:32:02 +0530

    aggressive cow problem

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 // int bs(vector<int>arr,int tar){
 //     int st=0,end=arr.size()-1;
@@ -37,7 +38,34 @@ using namespace std;
 //     }
 //     return ans;
 // }
-
+bool isPossible(vector<int>&arr,int n,int c,int minAllowedDist){
+    int cows=1,lastStallPos=arr[0];
+    for(int i=1;i<n;i++){
+        if(arr[i]-lastStallPos>=minAllowedDist){
+            cows++;
+            lastStallPos=arr[i];
+        }
+        if(cows==c){
+            return true;
+        }
+    }
+    return false;
+}
+int getDistance(vector<int>&arr,int n,int c){
+    sort(arr.begin(),arr.end());
+    int st=1,end=arr[n-1]-arr[0],ans=-1;
+    while(st<=end){
+        int mid=st+(end-st)/2;
+        if(isPossible(arr,n,c,mid)){
+            ans=mid;
+            st=mid+1;
+        }
+        else{
+            end=mid-1;
+        }
+    }
+    return ans;
+}
 int main(){;
     // vector<int>arr={2,4,6,7,9,10};
     // cout<<bs(arr,7);
@@ -46,6 +74,7 @@ int main(){;
     // int m = 2;
     // int result = allocateBooka(arr, n, m);
     // cout <<result;
-    vector<int>arr={};
+    vector<int>arr={1,2,8,4,9};
+    cout<<getDistance(arr,5,3);
     return 0;
 }
