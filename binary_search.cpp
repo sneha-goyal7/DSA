1cfd57880a085f5a6b5cb2802020c21234924a41
sneha-goyal7 <snehagoyal4295@gmail.com>
2026-09-20 13:03:18 +0530

    ppp

--- binary_search.cpp
+++ binary_search.cpp
@@ -11,38 +11,41 @@ using namespace std;
 //     }
 //     return -1;
 // }
-bool isValid(vector<int>&arr,int n,int m,int maxAllowedPages){
-    int student=1,pages=0;
-    for(int i=0;i<n;i++){
-        if(arr[i]>maxAllowedPages) return false;
-        if(pages+arr[i]<=maxAllowedPages) pages+=arr[i];
-        else {student++;
-        pages=arr[i];}
-    }
-    return student<=m;
-}
-int allocateBooka(vector<int>&arr,int n,int m){
-    if(m>n)return -1;/////here is the last condition base case
-    int ans=-1;
-    int st=0,end=0;
-    for (int i = 0; i < n; i++) end += arr[i]; 
-    while(st<=end){
-        int mid=st+(end-st)/2;
-        if(isValid(arr,n,m,mid)){
-            ans=mid;
-            end=mid-1;
-        } 
-        else st=mid+1;
-    }
-    return ans;
-}
+// bool isValid(vector<int>&arr,int n,int m,int maxAllowedPages){
+//     int student=1,pages=0;
+//     for(int i=0;i<n;i++){
+//         if(arr[i]>maxAllowedPages) return false;
+//         if(pages+arr[i]<=maxAllowedPages) pages+=arr[i];
+//         else {student++;
+//         pages=arr[i];}
+//     }
+//     return student<=m;
+// }
+// int allocateBooka(vector<int>&arr,int n,int m){
+//     if(m>n)return -1;
+//     /////here is the last condition base case
+//     int ans=-1;
+//     int st=0,end=0;
+//     for (int i = 0; i < n; i++) end += arr[i]; 
+//     while(st<=end){
+//         int mid=st+(end-st)/2;
+//         if(isValid(arr,n,m,mid)){
+//             ans=mid;
+//             end=mid-1;
+//         } 
+//         else st=mid+1;
+//     }
+//     return ans;
+// }
+
 int main(){;
     // vector<int>arr={2,4,6,7,9,10};
     // cout<<bs(arr,7);
-    vector<int> arr = {12, 34, 67, 90};
-    int n = arr.size();
-    int m = 2;
-    int result = allocateBooka(arr, n, m);
-    cout <<result;
+    // vector<int> arr = {12, 34, 67, 90};
+    // int n = arr.size();
+    // int m = 2;
+    // int result = allocateBooka(arr, n, m);
+    // cout <<result;
+    vector<int>arr={};
     return 0;
 }
